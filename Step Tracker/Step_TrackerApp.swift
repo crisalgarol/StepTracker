44823364a67854fa3ac2a25ab72922f1baf60b6a
Cristian Salomón Olmedo <38919041+crisalgarol@users.noreply.github.com>
2025-02-18 21:01:41 -0600

    Created Xcode Project

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Cristian Olmedo on 18/02/25.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
